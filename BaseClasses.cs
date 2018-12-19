a762ac1437af105f425335049f8602a984432b9d
Maura Cerioli <maura.cerioli@unige.it>
2018-12-19 19:17:34 +0100

    Add resources for the project
    AuctionSite_refman.pdf	documentation of the interfaces to be used and those to be implemented
    AuctionSite_BasicTest_refman.pdf	documentation of the minimal set of tests which must be passed to have an acceptable project
    TAP2018-19_AuctionSite.AuctionSite.Testing.zip	sources of the minimal set of tests
    BaseClasses.cs	test base classes (basic configuration of testing environment)
    TAP2018-19_AuctionSite.AlarmClock.Interface.pdb	interface of the alarm clock, to be used (symbols for debugging)
    TAP2018-19_AuctionSite.AlarmClock.Interface.dll	interface of the alarm clock, to be used
    TAP2018-19_AuctionSite.AuctionSite.Interface.pdb	interface of the auction site, to be implemented (symbols for debugging)
    TAP2018-19_AuctionSite.AuctionSite.Interface.dll	interface of the auction site, to be implemented

---
+++ BaseClasses.cs
@@ -0,0 +1,61 @@
+﻿using TAP2018_19.AlarmClock.Interfaces;
+using TAP2018_19.TestBaseClasses;
+
+namespace TAP2018_19.TestBaseClasses {
+    using System;
+    using System.Diagnostics;
+    using AlarmClock.Interfaces;
+    using AuctionSite.Interfaces;
+    using NUnit.Framework;
+    using Ninject;
+
+    internal static class Configuration {
+        internal const string ImplementationAssembly =
+            @"..\..\..\NAME OF YOUR IMPLEMENTATION PROJECT FOLDER\bin\Debug\NAME OF YOUR IMPLEMENTATION PROJECT LIBRARY";
+
+        internal const string ConnectionString =
+            @"Data Source=.\SQLEXPRESS;Initial Catalog=ANY NAME YOU LIKE;Integrated Security=True;";
+    }
+
+    [TestFixture]
+    public abstract class AbstractTest {
+        protected static readonly IAlarmClockFactory AnAlarmClockFactory;
+        protected static readonly ISiteFactory AnAuctionSiteFactory;
+        protected static readonly string ImplementationAssembly = Configuration.ImplementationAssembly;
+
+        public static ISiteFactory LoadSiteFactoryFromModule() {
+            var kernel = new StandardKernel();
+            ISiteFactory result = null;
+            try {
+                kernel.Load(Configuration.ImplementationAssembly);
+                result = kernel.Get<ISiteFactory>();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+            return result;
+        }
+
+        static AbstractTest() {
+            var kernel = new StandardKernel();
+
+            try {
+                AnAuctionSiteFactory = LoadSiteFactoryFromModule();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    public abstract class AuctionSiteTest : AbstractTest {
+        protected ISiteFactory GetSiteFactory() {
+            return AbstractTest.AnAuctionSiteFactory;
+        }
+
+        protected string GetConnectionString() {
+            return Configuration.ConnectionString;
+        }
+    }
+}
